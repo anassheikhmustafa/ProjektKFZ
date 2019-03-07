789a5b56704988e6844cf96fc40bdec43d8fcb69
Kronreif <48323192+Kronreif@users.noreply.github.com>
2019-03-07 16:26:37 +0100

    Upload

--- projekt/teile/teile.php
+++ projekt/teile/teile.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html><html lang="de"><head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <link href="../style.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
@@ -90,16 +91,18 @@ function myFunction() {
 <h2>Aktionen:</h2>
 <p>
 <input type="submit" name="eintragen" formaction="teileeintragen.php" value="Neuen Artikel eintragen">
-</p>
-<p>
 <input type="submit" name="bearbeiten" formaction="teilebearbeiten.php" value="ausgewählten Datensatz bearbeiten">
-</p>
-<p>
 <input type="submit" name="löschen" formaction="teileloeschen.php" value="ausgewählte Datensätze löschen">
 </p>
 <br>
 </div>
 
 </form>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
 </body>
 </html>
